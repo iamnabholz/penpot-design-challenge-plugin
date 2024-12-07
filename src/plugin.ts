f397f9e5a63d191534434984da9496b42760de9d
Lukas Nabholz <lukas@nabholz.work>
2024-12-06 21:08:27 -0400

    enhanced prompt data. Added new prompts and locations, refined randomization

--- src/plugin.ts
+++ src/plugin.ts
@@ -5,13 +5,24 @@ penpot.ui.open("Design Challenge Generator", `?theme=${penpot.theme}`, {
 
 import prompt_design from "../public/challenge-design.json";
 import prompt_for from "../public/challenge-for.json";
+import prompt_locations from "../public/challenge-locations.json";
 
 penpot.ui.onMessage<string>((message) => {
   if (message === "generate") {
     const c_design =
       prompt_design[Math.floor(Math.random() * prompt_design.length)];
 
-    const c_for = prompt_for[Math.floor(Math.random() * prompt_for.length)];
+    let c_for = prompt_for[Math.floor(Math.random() * prompt_for.length)];
+
+    const locationRegex = /in\s[A-Z][a-z]+/;
+    if (locationRegex.test(c_for)) {
+      const locations = prompt_locations[Math.floor(Math.random() * prompt_locations.length)];
+      if (Math.random() >= 0.65) {
+        c_for = c_for.replace(locationRegex, `in ${locations}`);
+      } else {
+        c_for = c_for.replace(locationRegex, "");
+      }
+    }
 
     penpot.ui.sendMessage({
       message: "return",
